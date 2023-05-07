c9ce265a675e8e27a468a877cd5757af941f6956
Mr.Rovira <46496399+agustinroviraquezada@users.noreply.github.com>
2023-05-07 18:13:51 +0200

    Add files via upload

---
+++ cycle/DataMod.py
@@ -0,0 +1,131 @@
+#Necesary Packages
+import os
+import glob
+import os.path
+import numpy as np
+import pandas as pd
+import lightning.pytorch as pl
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader,Dataset
+from sklearn.model_selection import train_test_split
+import random
+import re
+
+
+##############################  Class 1 ############################## 
+class CustomDataset(Dataset):
+  def __init__(self, T1_files, T2_files):
+    """
+    @Description: 
+      A custom PyTorch Dataset class to  handle not paired T1 and T2 MRI image files.
+    @Input:
+        T1_files (List[str]): List of paths to T1 MRI image files.
+        T2_files (List[str]): List of paths to T2 MRI image files.
+    """
+    self.T1_files = T1_files
+    self.T2_files = T2_files
+
+  def __len__(self):
+    return min(len(self.T1_files), len(self.T2_files))
+
+  def __getitem__(self, idx):
+    """
+    @Description: 
+      Retrieves an item from the dataset at a specified index.
+    @Input:
+        idx (int): The index of the item to be retrieved.
+    @Output:
+        tuple: T1 image, T1 image's corresponding T2 image, T2 image, and T2 image's corresponding T1 image.
+    """
+
+    #Get Image Paths
+    img_path_T1 = self.T1_files[idx]
+    img_path_T2 = self.T2_files[idx]
+
+    #The images are not pairs! however, I want to find the pair to compare
+    img_path_T1_T2Corrs = re.sub(r'(T1)(w?)', r'T2\2', img_path_T1)
+    img_path_T2_T1Corrs = re.sub(r'(T2)(w?)', r'T1\2', img_path_T2)
+
+    #Load
+    img_T1 = torch.load(img_path_T1)
+    img_T2 = torch.load(img_path_T2)
+
+    img_T1_T2Corrs = torch.load(img_path_T1_T2Corrs)
+    img_T2_T1Corrs = torch.load(img_path_T2_T1Corrs)
+
+    return img_T1, img_T1_T2Corrs, img_T2 ,img_T2_T1Corrs
+
+
+
+
+##############################  Class 2 ############################## 
+class CycleGANDataModule(pl.LightningDataModule):
+    """
+      @Description: 
+        Custom PyTorch Lightning DataModule for MRI image data, pairs of T1 and T2.
+      @Input:
+          paths (dict): A dictionary containing the paths to T1 and T2 directories.
+          batch_size (int) = 1: Batch size for data loading
+          num_workers (int) = 4 : Number of workers for data loading
+          train_ratio (float) = 0.7: The ratio of training data
+          test_size (float) 0.1: The ratio of test data
+          val_size (float) = 0.2:   The ratio of validation data
+          seed (int) = 478: Seed for random number generation. 
+          factor (int) = 1: Factor for modifying the dataset.
+    """
+    
+    def __init__(self, paths,
+      batch_size=1,
+      num_workers=4,
+      train_ratio=0.7,
+      test_size=0.1,
+      val_size=0.2,
+      seed=478,
+      factor=1):
+
+
+      super().__init__()
+      self.T1_dir = paths["T1"]
+      self.T2_dir =  paths["T2"]
+      self.batch_size = batch_size
+      self.num_workers = num_workers
+      self.train_ratio = train_ratio
+      self.test_size = test_size
+      self.val_size = val_size
+      self.seed=seed
+      self.factor=factor
+      self.save_hyperparameters()
+
+
+
+    def prepare_data(self):
+      pass
+
+    def setup(self, stage=None):
+      #Get shuffled list of dirs in T1 and T2
+      T1_file = [os.path.join(self.T1_dir, f) for f in os.listdir(self.T1_dir) if f.endswith('.pt')]
+      T2_file = [os.path.join(self.T2_dir, f) for f in os.listdir(self.T2_dir) if f.endswith('.pt')]
+
+      T1_shuffled = random.sample(T1_file, len(T1_file))
+      T2_shuffled = random.sample(T2_file, len(T2_file))
+
+      #Split to validation, training and test
+      train_T1, test_T1, train_T2, test_T2 = train_test_split(T1_shuffled,T2_shuffled,test_size=self.test_size ,shuffle=True,random_state=self.seed)
+      train_T1, val_T1, train_T2, val_T2 = train_test_split(train_T1,train_T2,test_size=(self.val_size / (1-self.test_size)), random_state=self.seed)
+
+
+      self.train_dataset = CustomDataset(train_T1, train_T2)
+      self.val_dataset = CustomDataset(val_T1, val_T2)
+      self.test_dataset = CustomDataset(test_T1, test_T2)
+
+    def train_dataloader(self):
+      return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
+
+    def val_dataloader(self):
+      return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+      return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+
+  
---
+++ cycle/Get_Data.py
@@ -0,0 +1,196 @@
+import requests
+import os
+import re
+import gzip
+import shutil
+
+
+class Import_nii:
+    def __init__(self, dataset_id, version, save_path):
+        """
+          @Description
+            The Import_nii class provides a way to download NIfTI files from the OpenNeuro dataset.
+            It fetches the required dataset based on the dataset ID and version, filters the relevant files,
+            and downloads them to the specified local path. The downloaded files are uncompressed and saved
+            as .nii files.
+
+
+          @Inputs
+            - dataset_id: The dataset ID. This is the name of the folder where the file is saved as well
+            - version: The version of the dataset.
+            - save_path: The local path to save the downloaded files. Parent folder for dataset_id
+            - group: Dataset group
+            - save_path_tensor: Final path where to store the .pt
+            - bound: Range where I am going to slice the .nii
+        """
+
+        self.dataset_id = dataset_id
+        self.version = version
+        self.save_path = save_path
+        self.api_url = 'https://openneuro.org/crn/graphql'
+
+
+        # Define the GraphQL query to get the dataset snapshot files
+        self.query = f'''
+    {{
+      snapshot(datasetId: "{self.dataset_id}", tag: "{self.version}") {{
+        id
+        tag
+        files {{
+          id
+          filename
+          size
+          urls
+        }}
+      }}
+    }}
+    '''
+
+        # Get the URL info from the query
+        response = requests.post(self.api_url, json={'query': self.query})
+        self.response_data = response.json()
+
+        # Get for each subject the URL of the directory "anat"
+        subjects = {i['filename']: i['id'] for i in self.response_data['data']['snapshot']['files'] if
+                    "sub" in i['filename']}
+        Sub_anat = {k: self.RecQuery(v) for k, v in subjects.items()}
+
+        # Save the files  in a dictionary such as  'sub-001_T2w.nii': URL
+        toDownload = {}
+        for k, v in Sub_anat.items():
+            toDownload.update(self.GetURL(v))
+
+        # Download the files
+        for k, v in toDownload.items():
+            self.Download(k, v)
+
+    def RecQuery(self, id):
+        """
+        @Description
+          Perform a recursive GraphQL query for the given file ID to find the corresponding 'anat' file.
+
+        @Inputs
+          - id (str): The file ID to perform the recursive query on.
+
+        @output
+            - file_id (str): The file ID of the corresponding 'anat' file.
+        """
+
+        query = f'''
+    {{
+      snapshot(datasetId: "{self.dataset_id}", tag: "{self.version}") {{
+        files(tree: "{id}") {{
+          id
+          filename
+          size
+          urls
+        }}
+      }}
+    }}
+    '''
+
+        response = requests.post(self.api_url, json={'query': query})
+        response_data = response.json()
+        response_data = response_data['data']['snapshot']['files']
+        file_id = None
+
+        for i in response_data:
+            if 'anat' in i['filename']:
+                file_id = i['id']
+
+        return file_id
+
+    def extract_subject_modality(self, url):
+        """
+        @Description
+          Extract subject number and modality from the given URL.
+
+        @Inputs
+          - url (str): The URL to extract the subject number and modality from.
+
+        @output
+          - result (str): The formatted string containing subject number and modality, e.g., 'sub-001_T2w.nii'.
+        """
+
+        # Extract subject number and modality from the URL
+        subject_match = re.search(r'sub-(\d+)', url)
+        modality_match = re.search(r'sub-\d+_([a-zA-Z0-9]+)', url)
+        result = None
+
+        if subject_match and modality_match:
+            subject_number = int(subject_match.group(1))
+            subject = f'sub-{subject_number:03d}'
+            modality = modality_match.group(1)
+            result = f'{subject}_{modality}.nii'
+
+        return result
+
+    def GetURL(self, ant_id):
+        """
+        @Description
+          Perform a GraphQL query for the given 'anat' file ID to find the corresponding NIfTI file URLs.
+
+        @Inputs
+          - ant_id (str): The 'anat' file ID.
+
+        @output
+          - url_dict (dict): A dictionary containing the formatted string of subject and modality as keys,
+                            and corresponding NIfTI file URLs as values.
+        """
+
+        query = f'''
+      {{
+        snapshot(datasetId: "{self.dataset_id}", tag: "{self.version}") {{
+          files(tree: "{ant_id}") {{
+            id
+            filename
+            size
+            urls
+          }}
+        }}
+      }}
+      '''
+
+        response = requests.post(self.api_url, json={'query': query})
+        response_data = response.json()
+        response_data = response_data['data']['snapshot']['files']
+
+        return {self.extract_subject_modality(i["urls"][0]): i["urls"][0] for i in response_data if
+                ".nii.gz" in i["urls"][0]}
+
+    def Download(self, filename, url):
+        """
+        @Description
+          Download the NIfTI file from the given URL, uncompress it, and save it in the specified path.
+
+        @Inputs
+          - filename (str): The filename of the NIfTI file to be saved.
+          - url (str): The URL to download the NIfTI file from.
+
+        @output
+          - Downloads the NIfTI file, uncompresses it, and saves it in the specified path.
+          - Removes the compressed .gz file after uncompressing it.
+        """
+
+        # Create a directory for the downloaded files
+        download_directory = os.path.join(self.save_path, f'{self.dataset_id}_files')
+        os.makedirs(download_directory, exist_ok=True)
+
+        local_file_path_nii = os.path.join(download_directory, filename)
+        local_file_path = local_file_path_nii + '.gz'
+
+        response = requests.get(url)
+
+        with open(local_file_path, 'wb') as f:
+            f.write(response.content)
+        print(f'Downloaded {filename} to {local_file_path}')
+
+        # Uncompress the .gz file and save it as a .nii file
+        with gzip.open(local_file_path, 'rb') as f_in:
+            with open(local_file_path_nii, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        print(f'Uncompressed {filename} to {local_file_path_nii}')
+
+        # Remove the .gz file
+        os.remove(local_file_path)
+        print(f'Removed {local_file_path}')
