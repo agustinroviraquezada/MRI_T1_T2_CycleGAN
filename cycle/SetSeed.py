952b3a11c8ed1a21e530f705f2f4ab307da0612a
Mr.Rovira <46496399+agustinroviraquezada@users.noreply.github.com>
2023-05-13 13:26:09 +0200

    Update SetSeed.py

--- cycle/SetSeed.py
+++ cycle/SetSeed.py
@@ -5,6 +5,18 @@ import numpy as np
 import torch
 
 def seed_everything(seed=478):
+  """
+  Seed the random number generators for reproducibility.
+
+  @Description:
+    The seed_everything function sets the seed value for various random number generators to achieve reproducibility in a program. It sets the seed value for the random, numpy, torch CPU, and torch CUDA random number generators.
+
+  @Input:
+    - seed (int, optional): The seed value to set for the random number generators. Defaults to 478.
+
+  @Output:
+    - int: The seed value that was set for the random number generators.
+  """
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
