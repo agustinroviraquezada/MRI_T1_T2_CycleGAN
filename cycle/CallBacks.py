11cccd42fcc0b5a069205afb0082ae95e7c47084
Mr.Rovira <46496399+agustinroviraquezada@users.noreply.github.com>
2023-05-13 15:43:19 +0200

    Update CallBacks.py

--- cycle/CallBacks.py
+++ cycle/CallBacks.py
@@ -7,6 +7,18 @@ import torchvision
 ##############################  Class 1 ################################
 
 class CustomModelCheckpoint(pl.Callback):
+   """
+    @Description:
+      This callback extends the functionality of the PyTorch Lightning ModelCheckpoint callback. It saves the best model based on a specified metric and also allows saving models periodically during training.
+
+    @Input:
+      - monitor (str): The metric to monitor for saving the best model.
+      - save_path (str): The path where the model checkpoints will be saved.
+      - save_best (str): The path where the best model checkpoint will be saved.
+      - version (int): The version number to append to the saved model checkpoints.
+      - save_top_k (int): The maximum number of best models to keep.
+      - every_n_epochs (int): Save a model checkpoint every N epochs.
+  """
   def __init__(self, monitor, save_path,save_best,version,save_top_k=3,every_n_epochs=None):
     super().__init__()
     self.monitor = monitor
@@ -21,6 +33,14 @@ class CustomModelCheckpoint(pl.Callback):
 
 
   def on_validation_epoch_end(self, trainer, pl_module):
+    """
+    @Description:
+      This function is called at the end of each validation epoch. It saves the best model based on the monitored metric and periodically saves models during training.
+
+    @Input:
+      - trainer (pytorch_lightning.trainer.Trainer): The trainer object.
+      - pl_module (pytorch_lightning.LightningModule): The PyTorch Lightning module.
+    """
     current_value = trainer.logged_metrics.get(self.monitor)
 
     if len(self.best_k_models) < self.save_top_k or current_value > self.kth_best_value:
@@ -37,6 +57,14 @@ class CustomModelCheckpoint(pl.Callback):
       self.kth_best_value = min(list(self.best_k_models.values()))
 
   def on_train_epoch_end(self, trainer, pl_module):
+    """
+    @Description:
+      This function is called at the end of each training epoch. It saves a model checkpoint every N epochs, where N is the specified value in the 'every_n_epochs' parameter.
+
+    @Input:
+      - trainer (pytorch_lightning.trainer.Trainer): The trainer object.
+      - pl_module (pytorch_lightning.LightningModule): The PyTorch Lightning module.
+    """
     if self.every_n_epochs is not None and trainer.current_epoch % self.every_n_epochs == 0:
       ckpt_path = os.path.join(self.save_path, f"model__version_{self.version}_{trainer.current_epoch:02d}.ckpt")
       trainer.save_checkpoint(ckpt_path)
@@ -46,6 +74,15 @@ class CustomModelCheckpoint(pl.Callback):
 ##############################  Class 2 ################################
 
 class CreateGIF(pl.Callback):
+   """
+  @Description:
+    This callback creates GIF images at the end of each validation epoch, showing the generated images and their reconstructions.
+
+  @Input:
+    - im_T1 (torch.Tensor): Input image from domain T1.
+    - im_T2 (torch.Tensor): Input image from domain T2.
+    - save_path (str): The path where the GIF images will be saved.
+  """
   def __init__(self, im_T1, im_T2, save_path):
     super().__init__()
     self.save_path = save_path
