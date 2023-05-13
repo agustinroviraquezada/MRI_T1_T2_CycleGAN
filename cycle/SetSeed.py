9e76302e7ae3a527730c1daddd2da6199836a3de
Mr.Rovira <46496399+agustinroviraquezada@users.noreply.github.com>
2023-05-13 17:40:17 +0200

    Update SetSeed.py

--- cycle/SetSeed.py
+++ cycle/SetSeed.py
@@ -17,11 +17,11 @@ def seed_everything(seed=478):
   @Output:
     - int: The seed value that was set for the random number generators.
   """
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
-    return seed
+  random.seed(seed)
+  os.environ['PYTHONHASHSEED'] = str(seed)
+  np.random.seed(seed)
+  torch.manual_seed(seed)
+  torch.cuda.manual_seed(seed)
+  torch.backends.cudnn.deterministic = True
+  torch.backends.cudnn.benchmark = True
+  return seed
