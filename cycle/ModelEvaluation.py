c89abf026e4e44c6610432413d85d8d7e31d8e7b
Mr.Rovira <46496399+agustinroviraquezada@users.noreply.github.com>
2023-05-22 12:26:06 +0200

    Update ModelEvaluation.py

--- cycle/ModelEvaluation.py
+++ cycle/ModelEvaluation.py
@@ -67,10 +67,10 @@ class ModelEval():
     fl,cl=[0,0,1,1],[0,2,0,2]
     for t2,t2f,f,c in zip(T2_sample,T2f_sample,fl,cl):
       ax[f,c]=plt.imshow(t2,cmap="gray")
-      ax[f,c]
+      ax[f,c].set_title("Real")
 
       ax[f,c+1]=plt.imshow(t2f,cmap="gray")
-      ax[f,c+1]
+      ax[f,c+1].set_title("False")
     plt.axis('off')
     plt.savefig("Plot_SampleEval.png", format='png')
     plt.show()
